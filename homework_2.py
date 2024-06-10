fbd7cce6c52e382bff0bcbc63df02c8ebca38588
МишаняКудесник <mischukow1903@gmail.com>
2024-06-10 18:25:36 +0300

    дз

---
+++ homework_2.py
@@ -0,0 +1,5 @@
+a = input()
+if a.count('@') == 1 and a.count('.') == 1:
+  print(""" "Python is a program language!", said Jon. "I don't ever remember having this much fun before!!!" """)
+else:
+  print('No, no, no! "Python is a program language!"')
